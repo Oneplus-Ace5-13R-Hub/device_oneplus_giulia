acb21c2fa3e8273f4b77854c2b91942c1ce72e21
chuspita <kosecki99@gmail.com>
2026-09-08 12:09:15 +0200

    giulia: Set TARGET_DISABLE_EPPE as true
    Change-Id: Ia2dab50062f7c634ee67fb5e239e5b779b1a740c

--- lineage_giulia.mk
+++ lineage_giulia.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from giulia device
 $(call inherit-product, device/oneplus/giulia/device.mk)
 
+# Disable EPPE
+TARGET_DISABLE_EPPE := true
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 

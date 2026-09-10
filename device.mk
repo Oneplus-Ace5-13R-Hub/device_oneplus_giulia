aosp-for-giulia/android_device_oneplus_giulia

giulia: Add support for OPlus Camera

device.mk
@@ -43,6 +43,9 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
+# OPlus Camera
+$(call inherit-product-if-exists, vendor/oplus/camera/giulia/opluscamera.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage

Oneplus-Ace5-13R-Hub/device_oneplus_giulia

Change-Id: Ia65d90843b0af84265f7116709ad93f83ebc7740

device.mk
@@ -39,6 +39,9 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
+# OPlus Camera
+$(call inherit-product-if-exists, vendor/oplus/camera/giuliac/opluscamera.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage